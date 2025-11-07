5b9caf8ebecc226e2f5549c44109dc6101ea78dc
edvino <edvin.olofsson@matfys.lth.se>
2025-11-07 23:40:23 +0100

    Add test runner and interpolation test module
    There is now a program to run test cases. The testing procedure is
    implemented using the test-drive framework. A module containing tests
    for the interpolation module has been implemented. It currently checks
    that the interpolants agree with the function samples within a set
    tolerance. Also checks that MTT_interp can handle stage (b) termination
    (overcomplete problem) properly.

--- test/check.f90
+++ test/check.f90
@@ -1,5 +1,29 @@
 program check
+use iso_fortran_env, only: stdout => output_unit, stderr => error_unit
+use testdrive, only: run_testsuite, new_testsuite, testsuite_type
+use test_interpolation, only: collect_interpolation_suite
 implicit none
+integer :: stat,is
+type(testsuite_type), allocatable :: testsuites(:)
+character(len=*), parameter :: fmt = '("#", *(1x, a))'
 
-print *, "Put some tests in here!"
+stat = 0
+testsuites = [&
+    new_testsuite("Interpolation test suite",collect_interpolation_suite)&
+    ]
+
+write(stdout,'(a)') 'Running tests!'
+
+do is = 1, size(testsuites)
+    write(stderr,fmt) "Testing:", testsuites(is)%name
+    call run_testsuite(testsuites(is)%collect, stdout, stat)
+end do
+
+if (stat > 0) then
+    write(stderr,'(i0,1x,a)') stat, "test(s) failed!"
+    error stop
+else if (stat == 0) then
+    write(stdout,'(a)') 'All tests passed succesfully!'
+end if
+
 end program check
