d35a5ad7ffb43b20d1f6288179193bb8fecc6367
edvino <edvin.olofsson@matfys.lth.se>
2025-12-21 20:59:22 +0100

    Add polynomial_eval test suite
    Add a test suite for poly_eval and rat_eval.

--- test/check.f90
+++ test/check.f90
@@ -2,6 +2,7 @@ program check
 use iso_fortran_env, only: stdout => output_unit, stderr => error_unit
 use testdrive, only: run_testsuite, new_testsuite, testsuite_type
 use test_interpolation, only: collect_interpolation_suite
+use test_polynomial_eval, only: collect_polynomial_eval_suite
 implicit none
 integer :: stat,is
 type(testsuite_type), allocatable :: testsuites(:)
@@ -9,7 +10,8 @@ character(len=*), parameter :: fmt = '("#", *(1x, a))'
 
 stat = 0
 testsuites = [&
-    new_testsuite("Interpolation test suite",collect_interpolation_suite)&
+    new_testsuite("Interpolation test suite",collect_interpolation_suite),&
+    new_testsuite("Polynomial evaluation test suite",collect_polynomial_eval_suite)&
     ]
 
 write(stdout,'(a)') 'Running tests!'
